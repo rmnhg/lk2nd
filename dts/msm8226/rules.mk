84c30809fd0a146038b363130b495696d36a18a0
Ramón Hernández García <rmnhg16@gmail.com>
2022-08-20 13:56:04 +0200

    dts: msm8226: add support for Sony Xperia E3 This commit should support 4G and 3G models, but it has only been tested with a D2203 (4G) variant.
    Co-authored-by: infus38 <infus38@gmail.com>

--- dts/msm8226/rules.mk
+++ dts/msm8226/rules.mk
@@ -6,4 +6,6 @@ DTBS += \
 	$(LOCAL_DIR)/apq8026-lg-lenok.dtb \
 	$(LOCAL_DIR)/apq8026-samsung-r03.dtb \
 	$(LOCAL_DIR)/msm8226-samsung-ms013g.dtb \
-	$(LOCAL_DIR)/msm8926-huawei-g6-l11-vb.dtb
+	$(LOCAL_DIR)/msm8926-huawei-g6-l11-vb.dtb \
+	$(LOCAL_DIR)/msm8926-sony-flamingo-ap.dtb \
+	$(LOCAL_DIR)/msm8226-sony-flamingo-dp.dtb
